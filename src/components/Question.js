8a272ab0a01522468f2c9bd3c0403cd714e383eb
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-11-21 10:45:45 -0600

    completed phase2 react useeffect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      // Decrement timeRemaining by 1 every second
+      setTimeRemaining((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+    }, 1000);
+
+    // When timeRemaining reaches 0, reset it to 10 and call onAnswered with false
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    // Clear the interval when the component is unmounted or when timeRemaining changes
+    return () => clearTimeout(timerId);
+
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
